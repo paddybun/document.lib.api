e34b8026ab4279e54377b04ec0bafb1b36dc5ad3
Patrick Bunda <1834214+paddybun@users.noreply.github.com>
2023-07-30 14:41:10 +0200

    feat: added migration project

---
+++ document.lib.migrationtool/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using document.lib.shared.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((builder, config) =>
+    {
+        config.AddJsonFile("appsettings.json", false, false);
+        config.AddJsonFile("appsettings.Development.json", true, false);
+    })
+    .ConfigureServices((builder, config) =>
+    {
+        var cfgRoot = builder.Configuration;
+        config.Configure<AppConfiguration>(cfgRoot.GetSection("Config"));
+    })
+    .Build();
+
+var cfg = host.Services.GetService<IOptions<AppConfiguration>>();
+await host.StartAsync();
+
+Console.WriteLine("Started Host. Enter to exit!");
+Console.Read();
