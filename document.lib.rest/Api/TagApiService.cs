5beaf435de25e9e3c70f01f60d68093fecef38bc
Patrick Bunda <1834214+paddybun@users.noreply.github.com>
2024-05-29 12:01:53 +0200

    feat: finish cleanup

--- document.lib.rest/Api/TagApiService.cs
+++ document.lib.rest/Api/TagApiService.cs
@@ -4,41 +4,38 @@ using FluentValidation;
 
 namespace document.lib.rest.Api;
 
-internal sealed class TagApiService(ITagService tagService, IValidator<TagsGetParameters> getValidator, IValidator<TagsUpdateParameters> updateValidator, ApiConfig config)
+internal sealed class TagApiService(
+    ITagService tagService, 
+    IValidator<TagsGetParameters> getValidator, 
+    IValidator<TagsUpdateParameters> updateValidator)
 {
-    public async Task<IResult> GetTagsAsync(TagsGetParameters parameters, HttpContext http)
-    {
-        var validationResult = await getValidator.ValidateAsync(parameters);
-        if (!validationResult.IsValid)
-            return Results.BadRequest(validationResult.Errors.Select(x => x.ErrorMessage).ToArray());
-        
-        if (parameters.PageSize!.Value > config.MaxPageSize) 
-            return Results.BadRequest(string.Format(ErrorMessages.PageSizeExceeded, config.MaxPageSize));
-
-        var result = await tagService.GetTagsPagedAsync(parameters.Page!.Value, parameters.PageSize!.Value);
-        if (!result.IsSuccess)
-        {
-            return Results.StatusCode(500);
-        }
-        
-        var (count, pagedTags) = result.Data;
-        http.Response.Headers.Append("total-results", count.ToString());
-        return Results.Ok(pagedTags);
-    }
-
     public async Task<IResult> GetTagAsync(int id)
     {
+        if (id <= 0) 
+            return Results.BadRequest("Parameter 'id' must be greater than 0");
+        
         var result = await tagService.GetTagAsync(id);
         return result.IsSuccess 
             ? Results.Ok(result.Data!) 
             : Results.NotFound();
     }
 
+    public async Task<IResult> GetTagsAsync(TagsGetParameters parameters, HttpContext http)
+    {
+        if (ValidationHelper.Validate(getValidator, parameters) is { } validationResult) return validationResult;
+        
+        var result = await tagService.GetTagsPagedAsync(parameters.Page!.Value, parameters.PageSize!.Value);
+        if (!result.IsSuccess) 
+            return Results.StatusCode(500);
+        
+        var (count, pagedTags) = result.Data;
+        http.Response.Headers.Append("total-results", count.ToString());
+        return Results.Ok(pagedTags);
+    }
+
     public async Task<IResult> CreateTagsAsync(TagsUpdateParameters parameters)
     {
-        var validationResult = await updateValidator.ValidateAsync(parameters);
-        if (!validationResult.IsValid)
-            return Results.BadRequest(validationResult.Errors.Select(x => x.ErrorMessage).ToArray());
+        if (ValidationHelper.Validate(updateValidator, parameters) is { } validationResult) return validationResult;
         
         var models = parameters.DisplayNames.Select(x => new TagModel { Name = x, DisplayName = x }).ToList();
         var result = await tagService.CreateTagsAsync(models);
